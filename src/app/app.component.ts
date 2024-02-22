948b9b237f7e6c1ca99c0aded9be22b06557cb78
thetwai <thetwai2000@gmail.com>
2024-02-22 22:14:18 +0000

    Added services and components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroesComponent, MessagesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+  title = 'Tour of Heroes';
 }
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [CommonModule, FormsModule, HeroDetailComponent],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css'
+})
+export class HeroesComponent {
+  heroes: Hero[] = [];
+  selectedHero?: Hero;
+
+  constructor(private heroService: HeroService) {
+
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(items => {
+      this.heroes = items;
+    });
+  }
+}
